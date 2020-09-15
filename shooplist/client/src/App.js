17538e3b4793965f91d4fe97bd92e61ef0096470
Ana Sanz <35330393+Ahisanz@users.noreply.github.com>
2020-09-15 15:11:34 +0200

    add shopping components and trasition

---
+++ shooplist/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppNavbar from './components/AppNavbar';
+import ShoopingList from './components/ShoppingList'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar/>
+      <ShoopingList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ shooplist/client/src/components/AppNavbar.js
@@ -0,0 +1,46 @@
+/*jshint esversion: 6 */ 
+
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm" className="mb-5">
+                    <Container>
+                        <NavbarBrand href="">Shopping List</NavbarBrand>
+                        <NavbarToggler onClick={this.toggle}/>
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="mal-auto" navbar>
+                                <NavItem>
+                                    <NavLink href="https://github.com/Ahisanz">Github</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+
+export default AppNavbar;
