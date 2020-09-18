58511142e9e804f2823e1d65a945294aa8de787c
Ana Sanz <35330393+Ahisanz@users.noreply.github.com>
2020-09-18 17:18:21 +0200

    add redux to the client ready for backend connection

--- shooplist/client/src/App.js
+++ shooplist/client/src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar';
-import ShoopingList from './components/ShoppingList'
+import ShoopingList from './components/ShoppingList';
+import ItemModal from './components/itemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavbar/>
-      <ShoopingList/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar/>
+        <Container>
+          <ItemModal/>
+          <ShoopingList/>
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
